8539b155ed608c5af3ffce792dc2016b0f67dfa7
Riya rai <118008485+riya053@users.noreply.github.com>
2023-10-16 23:31:49 +0530

    Create Find minimum and maximum element in an array.cpp

---
+++ Find minimum and maximum element in an array.cpp
@@ -0,0 +1,44 @@
+Find minimum and maximum element in an array
+
+Given an array A of size N of integers. Your task is to find the minimum and maximum elements in the array.
+
+ 
+
+Example 1:
+
+Input:
+N = 6
+A[] = {3, 2, 1, 56, 10000, 167}
+Output: 1 10000
+Explanation: minimum and maximum elements of array are 1 and 10000.
+ 
+
+Example 2:
+
+Input:
+N = 5
+A[]  = {1, 345, 234, 21, 56789}
+Output: 1 56789
+Explanation: minimum and maximum element of array are 1 and 56789.
+
+
+  Solution:
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+   
+    long long max=a[0],min=a[0];
+    for(int i=0;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+        if(min>a[i])
+        {
+            min=a[i];
+        }
+    }
+    return make_pair(min,max);
+    
+    
+}
